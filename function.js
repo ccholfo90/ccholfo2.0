cfb7a7ceaca57ccdd468485079bf1efc0bcad21a
ccholfo90 <127322452+ccholfo90@users.noreply.github.com>
2023-03-30 16:21:01 -0300

    corrección

---
+++ function.js
@@ -0,0 +1,15 @@
+
+
+function cambiar(){
+    document.getElementById('titulo').innerHTML = "ezequielbuenzalida@gmail.com";
+    }
+    document.getElementById('boton').onclick = function() {
+    cambiar();
+    }
+ 
+    document.addEventListener('click', function(){
+        document.getElementById("demo").innerHTML = "Muchas gracias"
+    });
+
+    
+    
